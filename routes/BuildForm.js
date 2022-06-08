8437b2475cf6fdac10aa2c6c3a09dadd0524df2e
Arbaaz Ahmad <ArbaazA@bpl.netlink.com>
2022-06-08 18:45:33 +0530

    checks added

--- routes/BuildForm.js
+++ routes/BuildForm.js
@@ -6,22 +6,24 @@ const app=express()
 const NewForm=app.post('/createform',authenticate,(req,res)=>{
     const getForm=async ()=>{
         const userId= req.user
-        
         var ourDate = new Date();
         var pastDate = ourDate.getDate();
         ourDate.setDate(pastDate);
-       
-
-        const f1=new Form({
-            userId:userId,
-            formTitle:req.body.formTitle,
-            fields:req.body.fields,
-            createdOn:ourDate.toDateString()
-        })
-       const result=await f1.save();
-        res.json(result)
-
+        const found=await Form.findOne({formTitle:req.body.formTitle})
+        if(found){
+            res.json({message:"Form Title is already used"})
+        }
+        else{
+            const f1=new Form({
+                userId:userId,
+                formTitle:req.body.formTitle,
+                fields:req.body.fields,
+                createdOn:ourDate.toDateString()
+            })
+           const result=await f1.save();
+            res.json(result)
+        }
     }
-    getForm()
+ getForm()
 })
 module.exports=NewForm
